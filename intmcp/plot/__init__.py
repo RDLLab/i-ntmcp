427d68bdf2cf2802ccd581f0eb9b307eb5a75751
Jonathon <Jonathon.schwartz12@gmail.com>
2022-02-26 09:39:58 -0500

    removed unused plotting stuff

--- intmcp/plot/__init__.py
+++ intmcp/plot/__init__.py
@@ -4,11 +4,4 @@ from ._parts import import_results_df
 from ._parts import load_results
 from ._parts import results_summary
 from ._plot_categorical_line import plot_categorical_line
-from ._plot_pairwise_pis_bar import plot_seperate_pairwise_pis_bar
-from ._plot_pairwise_pis_bar import plot_pairwise_pis_bar
-from ._plot_pairwise_pis_by_rollout_pi_bar import \
-    plot_pairwise_pis_by_rollout_pis_bar
 from ._plot_pairwise_pis_y_by_x import plot_pairwise_pis_y_by_x
-from ._plot_pairwise_pis_y_by_x import plot_seperate_pairwise_pis_y_by_x
-from ._plot_y_by_nesting_level import plot_y_by_nesting_level
-from ._plot_y_by_policy import plot_y_by_policy
